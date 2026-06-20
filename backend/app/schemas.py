bd9ef58ccad34ed87a6f454b806ca0edf7a9e336
Sandeep Kumar V <112401041@smail.iitpkd.ac.in>
2026-06-20 20:37:13 +0530

    Added the schematics of the user response and user login

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,10 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,ConfigDict
 from datetime import date
 
 class UserCreate(BaseModel):
     name:str
     username:str
     email:str
+    password:str
+
+class UserResponse(BaseModel):
+    id: int
+    name:str
+    username:str
+    email:str
+
+    model_config = ConfigDict(from_attributes=True)
+
+class UserLogin(BaseModel):
+    username:str
     password:str
 
 class FlightTestCreate(BaseModel):
