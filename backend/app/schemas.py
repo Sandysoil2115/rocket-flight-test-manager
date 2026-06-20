f8d64506e104cef1d2271e9ffdd684a65b438d4c
Sandeep Kumar V <112401041@smail.iitpkd.ac.in>
2026-06-20 22:09:19 +0530

    Added flight test schematics for response and updating

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel,ConfigDict
-from datetime import date
+from datetime import date,datetime
 
 class UserCreate(BaseModel):
     name:str
@@ -20,11 +20,37 @@ class UserLogin(BaseModel):
     password:str
 
 class FlightTestCreate(BaseModel):
+    launch_date: date
     rocket_name: str
     rocket_weight: float
-    Date: date
     launch_angle: float
     no_of_motors: int
     thrust: float
     status: str
-    reason: str
+    reason: str | None = None
+    owner_id: int
+
+class FlightTestResponse(BaseModel):
+    id: int
+    launch_date: date
+    rocket_name: str
+    rocket_weight: float
+    launch_angle: float
+    no_of_motors: int
+    thrust: float
+    status: str
+    reason: str | None = None
+    owner_id: int
+    created_at : datetime
+
+    model_config = ConfigDict(from_attributes=True)
+    
+class FlightTestUpdate(BaseModel):
+    launch_date: date | None = None
+    rocket_name: str | None = None
+    rocket_weight: float | None = None
+    launch_angle: float | None = None
+    no_of_motors: int | None = None
+    thrust: float | None = None
+    status: str | None = None   
+    reason: str | None = None
