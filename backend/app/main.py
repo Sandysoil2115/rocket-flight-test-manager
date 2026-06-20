cd51753c1a1b65ded158c4ba32c954272bb71adb
Sandeep Kumar V <112401041@smail.iitpkd.ac.in>
2026-06-20 22:11:20 +0530

    Added the routes for the flight tests data

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Depends,HTTPException
 from sqlalchemy.orm import Session
-from app.schemas import UserCreate,UserResponse,UserLogin
-from app.crud import create_user,get_user_by_email,get_user_by_username
+from app.schemas import UserCreate,UserResponse,UserLogin,FlightTestCreate,FlightTestResponse,FlightTestUpdate
+from app.crud import create_user,get_user_by_email,get_user_by_username,create_flight_test,get_all_flight_tests,get_flight_test_by_id,update_flight_test
 from app.dependencies import get_db
 from app.auth import verify_password
 
@@ -31,3 +31,37 @@ def login(user:UserLogin,db: Session=Depends(get_db)):
     if not (verify_password(user.password,db_user.password_hash)):
         raise HTTPException(status_code=401,detail="Invalid Username or Password")
     return {"message":"success"}
+
+
+@app.post("/flight-tests",response_model=FlightTestResponse)
+def register_flight_Test(flight_test:FlightTestCreate,db:Session=Depends(get_db)):
+    return create_flight_test(db,flight_test)
+
+@app.get("/flight-tests",response_model=list[FlightTestResponse])
+def flights(db:Session=Depends(get_db)):
+    return get_all_flight_tests(db)
+
+@app.get("/flight-tests/{id}",response_model=FlightTestResponse)
+def getflight(id:int ,db: Session=Depends(get_db)):
+    flight_db = get_flight_test_by_id(db,id)
+    if flight_db is None:
+        raise HTTPException(status_code=404,detail="Flight test not found")
+    return flight_db
+
+@app.put("/flight-tests/{id}",response_model=FlightTestResponse)
+def update(id:int , flight: FlightTestUpdate, db: Session=Depends(get_db)):
+    flight_db = get_flight_test_by_id(db,id)
+    if flight_db is None:
+        raise HTTPException(status_code=404,detail="Flight test not found")
+    update_flight_test(flight,flight_db)
+    db.commit()
+    return flight_db
+
+@app.delete("/flight-tests/{id}")
+def remove(id:int , db: Session=Depends(get_db)):
+    flight_db = get_flight_test_by_id(db,id)
+    if flight_db is None:
+        raise HTTPException(status_code=404,detail="Flight test not found")
+    db.delete(flight_db)
+    db.commit()
+    return {"message":"Deleted the flight test successfully"}
