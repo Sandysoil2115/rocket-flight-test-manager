892052f8ace4951a2336cc7941557680caea62fb
Sandeep Kumar V <112401041@smail.iitpkd.ac.in>
2026-06-20 20:38:05 +0530

    Added the route for registering and login of the user

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI,Depends,HTTPException
+from sqlalchemy.orm import Session
+from app.schemas import UserCreate,UserResponse,UserLogin
+from app.crud import create_user,get_user_by_email,get_user_by_username
+from app.dependencies import get_db
+from app.auth import verify_password
 
 app = FastAPI()
 
@@ -9,3 +14,20 @@ def home():
 @app.get("/health")
 def health():
     return {"status":"running"}
+
+@app.post("/register",response_model=UserResponse)
+def register(user:UserCreate,db: Session=Depends(get_db)):
+    if(get_user_by_username(db,user.username)):
+        raise HTTPException(status_code=409,detail="Username already exists")
+    if(get_user_by_email(db,user.email)):
+        raise HTTPException(status_code=409,detail="Email already exists")    
+    return create_user(db,user)
+    
+@app.post("/login")
+def login(user:UserLogin,db: Session=Depends(get_db)):
+    db_user = get_user_by_username(db,user.username)
+    if(db_user is None):
+        raise HTTPException(status_code=401,detail="Invalid Username or Password")
+    if not (verify_password(user.password,db_user.password_hash)):
+        raise HTTPException(status_code=401,detail="Invalid Username or Password")
+    return {"message":"success"}
